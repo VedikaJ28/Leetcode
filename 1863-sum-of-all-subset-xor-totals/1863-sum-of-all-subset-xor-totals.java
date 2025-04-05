d20ff30af2645f3684ccb08142740ad977897e91
VedikaJ28 <150283797+VedikaJ28@users.noreply.github.com>
2025-04-06 04:19:54 +0530

    Time: 0 ms (100%), Space: 41.4 MB (24.86%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int total = 0;
+        for (int num : nums) {
+            total |= num;  // Step 1: Compute bitwise OR of all numbers
+        }
+        return total * (1 << (nums.length - 1));
+    }
+}
