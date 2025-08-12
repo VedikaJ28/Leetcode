e8dac95ad6e1ebdbcc8b573b56fabfc8a5338548
VedikaJ28 <150283797+VedikaJ28@users.noreply.github.com>
2025-08-12 22:34:56 +0530

    Time: 4 ms (97.74%), Space: 45 MB (43.98%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,26 +1,65 @@
+// class MinStack {
+//     Stack<List<Integer>> st;
+//     public MinStack() {
+//         st = new Stack<>();
+//     }
+    
+//     public void push(int val) {
+//         if(st.empty()) st.push(Arrays.asList(val,val));
+//         else{
+//             st.push(Arrays.asList(val,Math.min(val,st.peek().get(1))));
+//         }
+//     }
+    
+//     public void pop() {
+//         st.pop();
+//     }
+    
+//     public int top() {
+//         return st.peek().get(0);
+//     }
+    
+//     public int getMin() {
+//         return st.peek().get(1);
+//     }
+// }
+
 class MinStack {
-    Stack<List<Integer>> st;
+    Stack<Long> st;
+    long min;
     public MinStack() {
         st = new Stack<>();
+        min= Long.MAX_VALUE;
     }
     
     public void push(int val) {
-        if(st.empty()) st.push(Arrays.asList(val,val));
+        long v=val;
+        if(st.empty()) {st.push(v); min=v;}
         else{
-            st.push(Arrays.asList(val,Math.min(val,st.peek().get(1))));
+            if(v>=min) st.push(v);
+            else{
+                st.push((2*v)-min);
+                min=v;
+            }
         }
     }
     
     public void pop() {
+        if(st.empty()) return;
+        long n=st.peek();
         st.pop();
+        if(n<min) min=2*min-n;
     }
     
     public int top() {
-        return st.peek().get(0);
+        if(st.empty()) return -1;
+        long n=st.peek();
+        if(min<n) return (int)n;
+        return (int)min;
     }
     
     public int getMin() {
-        return st.peek().get(1);
+        return (int)min;
     }
 }
 
