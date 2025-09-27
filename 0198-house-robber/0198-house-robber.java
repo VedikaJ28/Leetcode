928717d36b7f0290333aa806ee91484ac31de3dd
VedikaJ28 <150283797+VedikaJ28@users.noreply.github.com>
2025-09-28 01:26:19 +0530

    Time: 0 ms (100%), Space: 41.3 MB (36%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,4 +1,5 @@
 class Solution {
+    /* MEMOIZATION
     public int rob(int[] nums) {
         int pick=0;
         int notPick=0;
@@ -6,6 +7,7 @@ class Solution {
         Arrays.fill(dp, -1);
         return check(nums.length-1, nums,dp, pick, notPick);
     }
+    
     private int check(int i, int []nums,int[] dp, int pick, int notPick){
         if(i==0) return nums[0];
         if(i<0) return 0;
@@ -16,5 +18,22 @@ class Solution {
         dp[i]= ans;
         return ans;
 
+    }
+    */
+    // TABULATION
+    public int rob(int[] nums) {
+        int pick=0;
+        int notPick=0;
+        int [] dp= new int[nums.length];
+        Arrays.fill(dp, -1);
+        dp[0]=nums[0];
+        int neg=0;
+        for(int i=1;i<nums.length;i++){
+            pick= nums[i];
+            if(i>1) pick+=dp[i-2];
+            notPick= dp[i-1];
+            dp[i] = Math.max(pick, notPick);
+        }
+        return dp[nums.length-1];
     }
 }
