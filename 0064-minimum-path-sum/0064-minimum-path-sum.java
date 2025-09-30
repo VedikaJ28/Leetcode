e9305023095730dcf5b82955ba3a31fec51594fb
VedikaJ28 <150283797+VedikaJ28@users.noreply.github.com>
2025-09-30 11:48:21 +0530

    Time: 4 ms (59.71%), Space: 47.6 MB (69.39%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,33 +1,52 @@
 class Solution {
-    public int minPathSum(int[][] grid) {
-        int sum=0;
+    // public int minPathSum(int[][] grid) {
+    //     //int sum=0;
+    //     int m = grid.length;
+    //     int n = grid[0].length;
+    //     int[][] dp = new int[m][n];
+    //     for(int i=0;i<m;i++){ 
+    //     Arrays.fill(dp[i], -1);
+    //     }
+
+    //     int count = check(grid, m - 1, n - 1, dp);
+
+    //     return count;
+    // }
+
+    // private int check(int[][] grid, int m, int n, int[][] dp) {
+      
+    //     if (m < 0 || n < 0){
+    //         return Integer.MAX_VALUE;
+    //     }
+    //     if(m==0 && n==0) return grid[0][0];
+    //     if (dp[m][n] != -1){
+    //         return dp[m][n];
+    //     }
+
+    //     int up =check(grid, m - 1, n, dp);
+    //     int left = check(grid, m, n - 1, dp);   
+
+    //     return dp[m][n] = grid[m][n] + Math.min(left,up);
+
+    // }
+
+    // TABULATION
+    public int minPathSum(int[][] grid){
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++){
-        
-        Arrays.fill(dp[i], -1);
+        dp[0][0]= grid[0][0];
+        for(int i=1;i<n;i++){
+            dp[0][i]=dp[0][i-1]+grid[0][i];
         }
-        int count = check(grid, m - 1, n - 1, dp);
-
-        return count;
-    }
-
-    private int check(int[][] grid, int m, int n, int[][] dp) {
-      
-        if (m < 0 || n < 0){
-            return Integer.MAX_VALUE;
+        for(int j=1;j<m;j++){
+            dp[j][0]=dp[j-1][0]+grid[j][0];
         }
-        if(m==0 && n==0) return grid[0][0];
-        if (dp[m][n] != -1){
-            return dp[m][n];
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]= grid[i][j]+Math.min(dp[i-1][j], dp[i][j-1]);
+            }
         }
-
-        int up =check(grid, m - 1, n, dp);
-        int left = check(grid, m, n - 1, dp);
-        
-
-        return dp[m][n] = grid[m][n] + Math.min(left,up);
-
+        return dp[m-1][n-1];
     }
 }
