a3629fecb45a8909fe531c5f329cba6bcaa2d2c9
VedikaJ28 <150283797+VedikaJ28@users.noreply.github.com>
2025-10-10 22:03:49 +0530

    Time: 5 ms (83.34%), Space: 43.5 MB (50.04%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,18 +1,34 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        
+        int n= nums.length;
         int total=0;
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<n;i++){
             total+=nums[i];
         }
         if((total-target) % 2 ==1 || total<target) return 0;
         int newTarget= (total-target)/2;
         
-        int[][] dp= new int[nums.length][newTarget+1];
-        for(int i=0;i<nums.length;i++){
-            Arrays.fill(dp[i],-1);
+        int[][] dp= new int[n][newTarget+1];
+        // for(int i=0;i<n;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return check(n-1, newTarget, nums, dp);
+        // for(int i=0;i<n;i++){
+        //     dp[i][0]= 1;
+        // }
+        // if(nums[0]<=newTarget) dp[0][nums[0]]=1;
+        if(nums[0]==0) dp[0][0]=2;
+        else dp[0][0]=1;
+        if(nums[0]!=0 && nums[0]<=newTarget) dp[0][nums[0]]=1;
+        for(int idx=1;idx<n;idx++){
+            for(int tar=0;tar<=newTarget;tar++){
+                int notPick= dp[idx-1][tar];
+                int pick=0;
+                if(nums[idx]<=tar) pick= dp[idx-1][tar-nums[idx]];
+                dp[idx][tar]= pick+notPick;
+            }
         }
-        return check(nums.length-1, newTarget, nums, dp);
+        return dp[n-1][newTarget];
     }
     private int check(int idx, int tar, int []nums, int[][] dp){
         if(idx==0){
