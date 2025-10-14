d3d9f05aeaf10eb81525c1e14d8446c1aa290d6d
VedikaJ28 <150283797+VedikaJ28@users.noreply.github.com>
2025-10-14 21:06:30 +0530

    Time: 14 ms (95.09%), Space: 51.1 MB (31.93%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -8,12 +8,13 @@ class Solution {
         // }
         // return check(text1, text2, m-1, n-1, dp);
         char text1_0= text1.charAt(0);
-        char text2_0= text2.charAt(0);        
-        for(int i=0;i<n;i++){
-            if(text2.charAt(i)==text1_0) dp[0][i]= 1;
+        char text2_0= text2.charAt(0);
+        if(text1_0==text2_0) dp[0][0]=1;        
+        for(int i=1;i<n;i++){
+            if(text2.charAt(i)==text1_0 || dp[0][i-1]==1) dp[0][i]= 1;
         }
-        for(int i=0;i<m;i++){
-            if(text1.charAt(i)==text2_0) dp[i][0]= 1;
+        for(int i=1;i<m;i++){
+            if(text1.charAt(i)==text2_0 || dp[i-1][0]==1) dp[i][0]= 1;
         }
         for(int i=1;i<m;i++){
             char text1_i= text1.charAt(i);
@@ -22,7 +23,7 @@ class Solution {
                 else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
             }
         }
-        System.out.print(dp.toString());
+        //System.out.print(dp.toString());
         return dp[m-1][n-1];
     }
     private int check(String a, String b, int idx1, int idx2, int[][] dp){
